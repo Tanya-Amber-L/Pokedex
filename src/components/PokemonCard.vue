d070662da65074ed35ac1ee97283fde25a78a142
Tanya-Amber-L <tanya.leenders@gmail.com>
2022-06-27 16:37:10 +0200

    type background color matches pokemon's type

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { def } from '@vue/shared';
+
+
     const props = defineProps({
         name: String,
         id: Number,
@@ -8,7 +11,28 @@
 
     let name = props.name.charAt(0).toUpperCase() + props.name.slice(1);
     let id = String(props.id).padStart(3, "0"); 
-    // let types = prop.types;
+    
+    const getAccordingBackground = (type) => {
+        switch (type) {
+            case "grass": return "bg-pokeGrass"
+            case "water": return "bg-pokeWater"
+            case "ghost": return "bg-pokeGhost"
+            case "fire": return "bg-pokeFire"
+            case "poison": return "bg-pokePoison"
+            case "flying": return "bg-pokeFlying"
+            case "ground": return "bg-pokeGround"
+            case "rock": return "bg-pokeRock"
+            case "bug": return "bg-pokeBug"
+            case "electric": return "bg-pokeElectric"
+            case "fairy": return "bg-pokeFairy"
+            case "fighting": return "bg-pokeFighting"
+            case "psychic": return "bg-pokePsychic"
+            case "steel": return "bg-pokeSteel"
+            case "ice": return "bg-pokeIce"
+            case "dragon": return "bg-pokeDragon"
+            default: return "bg-pokeNormal"
+        }
+    }
 
 </script>
 
@@ -20,10 +44,13 @@
     </div>
     <div class="">
 
-        <span v-for="type in props.types" class="rounded-full px-3 pb-1 ml-[2px] bg-slate-200 text-xs text-gray-50" >
-            {{type.type.name}}
+        <template v-for="type in props.types" >
 
-        </span>
+            <span class="rounded-full px-3 pb-1 ml-[2px] text-xs text-gray-50" :class="getAccordingBackground(type.type.name)">
+            {{type.type.name}}
+            </span>
+
+        </template>
     </div>
     <img src="../assets/right-arrow.png" alt="arrow" class="h-auto m-auto ml-2">
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /* eslint-env node */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				pokeGrass: "#95C24D",
+				pokeWater: "#4592C3",
+				pokeGhost: "#7B62A3",
+				pokeFire: "#FD7D25",
+				pokePoison: "#BA7EC8",
+				pokeFlying: "#2dd4e3",
+				pokeGround: "#d9b77e",
+				pokeRock: "#80612f",
+				pokeBug: "#78a150",
+				pokeElectric: "#e8b725",
+				pokeFairy: "#f390f5",
+				pokeFighting: "#fc3838",
+				pokePsychic: "#d12ca2",
+				pokeSteel: "#636362",
+				pokeIce: "#9fd2e3",
+				pokeDragon: "#4818d9",
+				pokeNormal: "#A3ACAE",
+			},
+		},
 	},
 	plugins: [],
 };
