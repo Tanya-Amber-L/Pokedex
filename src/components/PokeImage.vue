ba7ece02f03ac904f86f3916bbb802467f4c2086
Tanya-Amber-L <tanya.leenders@gmail.com>
2022-06-29 16:36:23 +0200

    added vue-inner-image-zoom library
    modified style to match the design. Could change the zoom box looks: secondary

--- src/components/PokeImage.vue
+++ src/components/PokeImage.vue
@@ -1,9 +1,35 @@
 <script setup>
+    import InnerImageZoom from 'vue-inner-image-zoom';
+    import 'vue-inner-image-zoom/lib/vue-inner-image-zoom.css';
+
     const props = defineProps({
         image: String
     })
 </script>
 
 <template>
-    <img class="w-3/4 m-auto" :src="props.image" alt="">
+    <div class="w-3/4 m-auto">
+        <inner-image-zoom 
+        :src="props.image"
+        :zoomSrc="props.image"
+        moveType="drag"
+    />
+    </div>
+    
 </template>
+
+
+<style>
+.iiz__btn.iiz__hint{
+    display: none;
+}
+.iiz__btn.iiz__close {
+    width: 35px;
+    height: 35px;
+    border-radius: 4px;
+}
+/* Custom style:
+- hide the magnifying glass button (works without, and is better with our style)
+- border radius on the close button, getting closer to our style too
+*/
+</style>
