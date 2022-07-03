42f78d1ee4c1c3d7c3ab06d625b9045e85dc7f4b
Tanya-Amber-L <tanya.leenders@gmail.com>
2022-07-03 12:26:14 +0200

    made component for single stat

--- src/components/PokeStats.vue
+++ src/components/PokeStats.vue
@@ -1,88 +1,57 @@
 <script setup>
-    const props = defineProps({
-        stats: Array
-    })
+import SingleStat from "./SingleStat.vue";
+const props = defineProps({
+  stats: Array,
+});
 
-    let hp = props.stats[0].base_stat
-    let attack = props.stats[1].base_stat
-    let defense = props.stats[2].base_stat
-    let spAttack = props.stats[3].base_stat
-    let spDefense = props.stats[4].base_stat
-    let speed = props.stats[5].base_stat
+let hp = props.stats[0].base_stat;
+let attack = props.stats[1].base_stat;
+let defense = props.stats[2].base_stat;
+let spAttack = props.stats[3].base_stat;
+let spDefense = props.stats[4].base_stat;
+let speed = props.stats[5].base_stat;
 
-    const statColor= (stat) => {
-        let allStats = []
-        props.stats.forEach(stat => {
-            allStats.push(stat.base_stat)
-        });
-        allStats.sort((a, b) => a - b)
-        let lowerStats = allStats.slice(0 ,3)
-        if (lowerStats.includes(stat)) {
-            return "bg-red-500"
-        }
-        return "bg-green-500"
-    }
-    
+const statColor = (stat) => {
+  let allStats = [];
+  props.stats.forEach((stat) => {
+    allStats.push(stat.base_stat);
+  });
+  allStats.sort((a, b) => a - b);
+  let lowerStats = allStats.slice(0, 3);
+  if (lowerStats.includes(stat)) {
+    return "bg-red-500";
+  }
+  return "bg-green-500";
+};
 </script>
 
 <template>
-    <section class="">
-        <h2 class="font-bold text-sm my-2">STATISTIEKEN</h2>
-        <div class="bg-white shadow text-black py-4 px-5 rounded-lg">
-            <!-- <div class="flex" v-for="stat in stats">
-                <span>{{stat.stat.name}}</span>
-                {{stat.base_stat}}
-                <div class="w-full bg-gray-200 rounded-full h-1.5 dark:bg-gray-700">
-                    <div class="bg-gray-600 h-1.5 rounded-full dark:bg-gray-300" :style="'width:' + stat.base_stat/255*100 + '%'"></div>
-                </div>
-            </div> -->
-            <div class="flex justify-between items-center py-0.5">
-                <span class="text-gray-400 min-w-[65px] text-sm">HP</span> 
-                {{hp}}
-                <div class="w-full max-w-[170px] max-w-[170px] rounded-full h-1 dark:bg-gray-200 ml-5">
-                    <div :class="statColor(hp)" class="h-1 rounded-full" :style="'width:' + hp/255*100 + '%'"></div>
-                </div>
-            </div>
-            <div class="flex justify-between items-center py-0.5">
-                <span class="text-gray-400 min-w-[65px] text-sm">Attack</span> 
-                {{attack}}
-                <div class="w-full max-w-[170px] rounded-full h-1 dark:bg-gray-200 ml-5">
-                    <div :class="statColor(attack)" class="h-1 rounded-full" :style="'width:' + attack/255*100 + '%'"></div>
-                </div>
-            </div>
-            <div class="flex justify-between items-center py-0.5">
-                <span class="text-gray-400 min-w-[65px] text-sm">Defense</span> 
-                {{defense}}
-                <div class="w-full max-w-[170px] rounded-full h-1 dark:bg-gray-200 ml-5">
-                    <div :class="statColor(defense)" class="h-1 rounded-full" :style="'width:' + defense/255*100 + '%'"></div>
-                </div>
-            </div>
-            <div class="flex justify-between items-center py-0.5">
-                <span class="text-gray-400 min-w-[65px] text-sm">Sp. Atk</span> 
-                {{spAttack}}
-                <div class="w-full max-w-[170px] rounded-full h-1 dark:bg-gray-200 ml-5">
-                    <div :class="statColor(spAttack)" class="h-1 rounded-full" :style="'width:' + spAttack/255*100 + '%'"></div>
-                </div>
-            </div>
-            <div class="flex justify-between items-center py-0.5">
-                <span class="text-gray-400 min-w-[65px] text-sm">Sp. Def</span> 
-                {{spDefense}}
-                <div class="w-full max-w-[170px] rounded-full h-1 dark:bg-gray-200 ml-5">
-                    <div :class="statColor(spDefense)" class="h-1 rounded-full" :style="'width:' + spDefense/255*100 + '%'"></div>
-                </div>
-            </div>
-            <div class="flex justify-between items-center py-0.5">
-                <span class="text-gray-400 min-w-[65px] text-sm">Speed</span> 
-                {{speed}}
-                <div class="w-full max-w-[170px] rounded-full h-1 dark:bg-gray-200 ml-5">
-                    <div :class="statColor(speed)" class="h-1 rounded-full" :style="'width:' + speed/255*100 + '%'"></div>
-                </div>
-            </div>
-            <div class="flex items-center py-0.5">
-                <span class="text-gray-400 min-w-[65px] text-sm">Total</span> 
-                {{hp + attack + defense + spAttack + spDefense + speed}}
-            </div>
-        </div>
-    </section>
+  <section class="">
+    <h2 class="font-bold text-sm mb-2">STATISTIEKEN</h2>
+    <div class="bg-white shadow text-black py-4 px-5 rounded-lg">
+      <SingleStat :name="'HP'" :stat="hp" :color="statColor(hp)" />
+      <SingleStat :name="'Attack'" :stat="attack" :color="statColor(attack)" />
+      <SingleStat
+        :name="'Defense'"
+        :stat="defense"
+        :color="statColor(defense)"
+      />
+      <SingleStat
+        :name="'Sp. Atk'"
+        :stat="spAttack"
+        :color="statColor(spAttack)"
+      />
+      <SingleStat
+        :name="'Sp. Def'"
+        :stat="spDefense"
+        :color="statColor(spDefense)"
+      />
+      <SingleStat :name="'Speed'" :stat="speed" :color="statColor(speed)" />
+      <div class="grid grid-cols-4 py-0.5">
+        <span class="text-gray-400 min-w-[65px] text-sm">Total</span>
+        {{ hp + attack + defense + spAttack + spDefense + speed }}
+      </div>
+    </div>
+  </section>
 </template>
 <!-- width:{{stat.base_stat / 255 * 100}}% -->
---
+++ src/components/SingleStat.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+
+const prop = defineProps({
+  name: String,
+  stat: String,
+  color: String,
+});
+console.log(prop);
+</script>
+<template>
+  <!-- flex justify-between items-center -->
+  <div class="grid grid-cols-4 py-0.5 items-center">
+    <span class="text-gray-400 text-sm">{{ prop.name }}</span>
+    {{ prop.stat }}
+    <div class="col-start-3 col-end-5 rounded-full h-1 dark:bg-gray-200">
+      <div
+        :class="color"
+        class="h-1 rounded-full"
+        :style="'width:' + (prop.stat / 255) * 100 + '%'"
+      ></div>
+    </div>
+  </div>
+</template>
